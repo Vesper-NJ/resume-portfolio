bc52c1961c95f0ad25cecd1fde3680f9b77b1ab4
Vesper <zwnj94@gmail.com>
2026-03-19 16:26:47 +0800

    Create index.ts file to export all hooks

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,6 @@
+// Export all hooks
+
+export { default as useCustomHook1 } from './useCustomHook1';
+export { default as useCustomHook2 } from './useCustomHook2';
+export { default as useCustomHook3 } from './useCustomHook3';
+// Add more hooks as needed
